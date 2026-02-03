c821182f46648c28341a25c22880b41f96ccab97
Athanasios Gotsopoulos <tan@neuraldsp.com>
2026-02-03 16:01:28 +0200

    Added MuteEffect template

---
+++ Processors/MuteEffect.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class MuteEffect
+{
+public:
+    
+    MuteEffect(){};
+    ~MuteEffect(){};
+    
+    void prepare(double sampleRate, int samplesPerBlock)
+    {
+    }
+    void process(float * buffer, int numSamples)
+    {
+    }
+    
+private:
+};
